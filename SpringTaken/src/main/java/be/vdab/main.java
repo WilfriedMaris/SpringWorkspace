2db7ba573ea8739c90f47953806465960031aac8
Wilfried Maris <willymaris@hotmail.com>
2017-03-29 17:56:46 +0200

    Thuis -> backup voor testjes

--- SpringTaken/src/main/java/be/vdab/main.java
+++ SpringTaken/src/main/java/be/vdab/main.java
@@ -2,16 +2,21 @@ package be.vdab;
 
 import java.io.IOException;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import be.vdab.presentation.PersoonViewer;
+import be.vdab.presentation.PresentationConfig;
+import be.vdab.repositories.RepositoryConfig;
+import be.vdab.services.ServiceConfig;
 
 public class main {
 
 	public static void main(String[] args) {
 
-		try(ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("container.xml")){
+		try(AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(
+						new Class[] {PresentationConfig.class, 
+								RepositoryConfig.class, ServiceConfig.class})){
 			//onderstaande try/catch staan niet in cursus. Echter word er overal een IOException gethrowed. Deze moet gevangen worden.
 			try {
 				context.getBean(PersoonViewer.class).afbeelden();
