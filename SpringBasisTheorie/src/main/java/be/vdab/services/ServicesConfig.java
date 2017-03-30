217100a305be7d4134f1ecda4395b3196e187bd7
Wilfried Maris <willymaris@hotmail.com>
2017-03-30 15:44:10 +0200

    VDAB -> thuis 30/03/2017

--- SpringBasisTheorie/src/main/java/be/vdab/services/ServicesConfig.java
+++ SpringBasisTheorie/src/main/java/be/vdab/services/ServicesConfig.java
@@ -1,15 +1,9 @@
 package be.vdab.services;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import be.vdab.restclients.KoersenClient;
-
 @Configuration
+@ComponentScan
 public class ServicesConfig {
-	
-	@Bean
-	EuroService euroService(KoersenClient koersenClient){
-		return new EuroService(koersenClient);
-	}
 }
